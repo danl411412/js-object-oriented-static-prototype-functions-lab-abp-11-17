0fbd9044b45bab7219b8ee60c3656c2ffa5db1ae
danl411412 <daan.b.lee@gmail.com>
2017-12-05 11:42:00 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,8 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponses.BatchCreate(arr) {
+  return arr.map(function(emailEach) {
+    return IcebreakerResponse(emailEach);
+  })
+}
